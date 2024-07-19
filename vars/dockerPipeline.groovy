d529c0f50fe6a60229a95218f94a4e6037861f13
sumanth8686 <58932303+sumanth8686@users.noreply.github.com>
2024-07-19 19:13:07 +0530

    updated

--- vars/dockerPipeline.groovy
+++ vars/dockerPipeline.groovy
@@ -1,6 +1,6 @@
 // now we need to call docker method mentioned in src folder
 
-import com.i27academy.buid.docker
+import com.i27academy.build.docker
 
 def call(Map pipelineParams) {
     Docker docker = new Docker(this)
